c96207add19a8d503b5f35046d9aa9cb366a03a0
janrajanjan <jsodarbe@fit.edu.ph>
2026-09-15 17:37:04 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,10 +1,13 @@
 <?php
 $_SERVER['CI_ENVIRONMENT'] = 'development';
 
-// Define writable path to /tmp and create necessary cache directories
 define('WRITEPATH', '/tmp/');
+
 if (!is_dir('/tmp/cache')) {
     @mkdir('/tmp/cache', 0777, true);
+}
+if (!is_dir('/tmp/logs')) {
+    @mkdir('/tmp/logs', 0777, true);
 }
 
 require __DIR__ . '/../public/index.php';
