e4a1a7d2d0473a817e4944dc685a32b79af6e4a2
janrajanjan <jsodarbe@fit.edu.ph>
2026-09-15 17:45:12 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,11 +3,10 @@ $_SERVER['CI_ENVIRONMENT'] = 'development';
 
 define('WRITEPATH', '/tmp/');
 
-if (!is_dir('/tmp/cache')) {
-    @mkdir('/tmp/cache', 0777, true);
-}
-if (!is_dir('/tmp/logs')) {
-    @mkdir('/tmp/logs', 0777, true);
-}
+// Force-create temporary runtime directories in Vercel
+@mkdir('/tmp/cache', 0777, true);
+@mkdir('/tmp/logs', 0777, true);
+@mkdir('/tmp/uploads', 0777, true);
 
+// Bypass public rewrite rules and boot app directly
 require __DIR__ . '/../public/index.php';
