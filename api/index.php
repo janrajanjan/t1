563bd9fe7a5d451c1143b7c31a04dc1af8a65199
janrajanjan <jsodarbe@fit.edu.ph>
2026-09-15 17:29:10 +0800

    Set CI_ENVIRONMENT and define WRITEPATH
    Override writable directory path for Vercel Serverless.

--- api/index.php
+++ api/index.php
@@ -1,2 +1,6 @@
 <?php
+// Override CodeIgniter's writable directory path for Vercel Serverless
+$_SERVER['CI_ENVIRONMENT'] = 'development';
+define('WRITEPATH', '/tmp/');
+
 require __DIR__ . '/../public/index.php';
